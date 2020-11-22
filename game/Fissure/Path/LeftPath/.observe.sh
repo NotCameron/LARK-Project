d5dadebdb736c78e329e31a61204a3f8bc6cf581
NotCameron <73380062+NotCameron@users.noreply.github.com>
2020-11-22 18:01:27 -0500

    Update .observe.sh

--- game/Fissure/Path/LeftPath/.observe.sh
+++ game/Fissure/Path/LeftPath/.observe.sh
@@ -1,5 +1,5 @@
 echo ""
 echo "You entered the room on the Left,
 In the middle of the room stands a giant spider, is seems to be wandering the room aimlessly.
-It looks fairly weak"
+It looks fairly weak, but caught in it's web is the sword Doppy left behind"
 echo ""
