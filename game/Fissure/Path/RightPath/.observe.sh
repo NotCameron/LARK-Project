2cb202ab765f201fe9079c0891171b329306db3e
NotCameron <73380062+NotCameron@users.noreply.github.com>
2020-11-22 18:05:08 -0500

    Update .observe.sh

--- game/Fissure/Path/RightPath/.observe.sh
+++ game/Fissure/Path/RightPath/.observe.sh
@@ -1,8 +1,7 @@
 echo ""
 echo "You enter the torch-lit room,
 As you walk in, the piercing gaze sends shivers down your spine...
-what is that?
-The torch seems to have 't1' engraved in it...
-Meybe there is a chest around...
+What was that?
+Seems like nothing is in the room, but the dungeon is beginning to get to you.
 There are stairs that lead downwards"
 echo ""
