bac6b3e886ed3e5448f4ca447fdc1a31ee037948
nocoah22 <ncoley7895@floridapoly.edu>
2020-11-22 11:28:42 -0500

    Added secret torch chest, spider chest, new weapon, started 2nd floor

--- game/Fissure/Path/RightPath/.observe.sh
+++ game/Fissure/Path/RightPath/.observe.sh
@@ -1,3 +1,6 @@
+echo ""
 echo "You enter the torch-lit room,
 As you walk in, the piercing gaze sends shivers down your spine... what is that?
+The torch seems to have 't1' engraved in it...
 There are stairs that lead downwards"
+echo ""
